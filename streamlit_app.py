1d28c5cd76548f7400da2bbf2a507dcb0a286bce
Akashdeep Dam <61204253+akashreveluv@users.noreply.github.com>
2024-11-08 13:19:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,10 @@ with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/My-Data-Science-Projects/Penguin-Classification-ML-App/refs/heads/main/penguins_data.csv')
   df
   
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
