5ea5f9c0dfee29120b659bdc0c862b8376d7be5f
Akashdeep Dam <61204253+akashreveluv@users.noreply.github.com>
2024-11-08 13:12:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🐧 Penguin Detection App 🐧")
 
 st.info("This app detect the species of penguin according to given data")
+
+df = pd.read_csv('https://raw.githubusercontent.com/My-Data-Science-Projects/Penguin-Classification-ML-App/refs/heads/main/penguins_data.csv')
+
+df
