36d9724677ad15dbc4def635a9e797c87760c6d1
Akashdeep Dam <61204253+akashreveluv@users.noreply.github.com>
2024-11-08 14:52:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,7 @@ with st.sidebar:
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-with st.expander('Input features'):
-  st.write('**Input Data**')
+with st.expander('Input Data'):
   input_df
 
 encode = ['island', 'sex']
